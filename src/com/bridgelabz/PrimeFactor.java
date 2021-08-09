b92002f20b2ccf207e24f7a327691fdce799d96b
Megha <meghajagadale99@gmail.com>
2021-08-09 14:14:12 +0530

    Added: initial commit

--- src/com/bridgelabz/PrimeFactor.java
+++ src/com/bridgelabz/PrimeFactor.java
@@ -3,20 +3,37 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class PrimeFactor {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a Number: ");
-        int n = sc.nextInt();
-        int i = 2;
-        System.out.print("The Prime Factors of " + n + " are : ");
-        while (n > 1) {
+
+    public static void getPrimeFactor(int n) {
+        int flag = 0;
+        int check = 0;
+        int findFactor = 0;
+        for (int i = 2; i <= n / 2; i++) {
             if (n % i == 0) {
-                System.out.print(i + " ");
-                n = n / i;
-            } else
-                i++;
+                findFactor = i;
+                for (int j = 2; j <= findFactor / 2; j++) {
+                    if (findFactor % j == 0) {
+                        flag = 1;
+                        break;
+                    }
+                }
+                if (flag == 0) {
+                    System.out.println(findFactor);
+                    check = 1;
+                }
+            }
         }
-        System.out.println(" ");
+        if (check == 0)
+            System.out.println("No Prime Factor Found Except 1 and " + n);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number to find PrimeFactor: ");
+        int n = sc.nextInt();
         sc.close();
+        getPrimeFactor(n);
     }
 }
+
